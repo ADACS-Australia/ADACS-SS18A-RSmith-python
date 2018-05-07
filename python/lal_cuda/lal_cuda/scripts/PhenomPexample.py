0de45b8c0085f1de5c97c38a52745be1492844fc
Gregory Poole <gbpoole@gmail.com>
2018-05-07 15:12:23 +1000

    Significant update of the testing code for 'SimIMRPhenomPFrequencySequence'.

--- python/lal_cuda/lal_cuda/scripts/PhenomPexample.py
+++ python/lal_cuda/lal_cuda/scripts/PhenomPexample.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import numpy as np
 import pylab as plt
 import os
@@ -6,10 +7,12 @@ import git
 import glob
 import click
 import timeit
+import math
 
 sys.path.insert(0, "/home/gpoole/3rd_Party/lib/python2.7/site-packages")
 import lal
 import lalsimulation
+import lal_cuda
 
 # Find the project root directory
 git_repo = git.Repo(os.path.realpath(__file__), search_parent_directories=True)
@@ -32,7 +35,10 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('--n_timing', default=0)
-def PhenomPexample(n_timing):
+@click.option('--n_freq', default=0)
+@click.option('--write2stdout/--no-write2stdout', default=False)
+@click.option('--write2bin/--no-write2bin', default=False)
+def PhenomPexample(n_timing,n_freq,write2stdout,write2bin):
     """The script calls a higher level function in LALSuite.  The output is two
     binary arrays corresponding to the two outputs hp_val, hc_val from
     PhenomPCore. The outputs are arrays of complex numbers; one for each
@@ -62,8 +68,13 @@ def PhenomPexample(n_timing):
 
     flow, fhigh = 20, 100
 
-    freqs = np.linspace(flow, fhigh, (fhigh - flow) + 1)
+    if(n_freq<1):
+        freqs = np.linspace(flow, fhigh, (fhigh - flow) + 1)
+        n_freq=len(freqs)
+    else:
+        freqs = np.linspace(flow, fhigh, n_freq)
 
+    # Perform timing test if required
     if(n_timing>0):
         # Create a callable for timing purposes
         t = timeit.Timer(lambda: lalsimulation.SimIMRPhenomPFrequencySequence(
@@ -81,10 +92,15 @@ def PhenomPexample(n_timing):
                 1,
                 None
             ))
+        # Burning first call (to avoid CUDA context establishment, etc)
+        print("Burning first call: %f seconds."%(t.timeit(number=1)))
         # Print timing results
-        print("Timing of function: %.5f seconds."%(t.timeit(number=n_timing)))
+        print("Average timing of %d subsequent calls: %.5f seconds."%(n_timing,t.timeit(number=n_timing)/float(n_timing)))
 
-    print("Running SimIMRPhenomPFrequencySequence...")
+    # A Numpy array holding the input parameters
+    inputs_runtime = np.array([chi1_l,chi2_l,chip,thetaJ,m1SI,m2SI,distance,alpha0,phic,fref])
+
+    # Perform runtime call
     H=lalsimulation.SimIMRPhenomPFrequencySequence(
         freqs,
         chi1_l,
@@ -99,32 +115,93 @@ def PhenomPexample(n_timing):
         fref,
         1,
         None)
-    print("Done.")
-
     hp_val = H[0].data.data
     hc_val = H[1].data.data
 
-    # Write results to screen
-    for [f_i,hp_i,hc_i] in zip(freqs,hp_val,hc_val):
-        print(f_i,hp_i.real,hp_i.imag,hc_i.real,hc_i.imag)
+    # This flag declares whether the reference test dataset's inputs are the same as the run-time dataset
+    flag_valid_ref = True
 
-    # Write inputs to binary file
-    inputs_file  = open("./inputs.dat", "wb")
-    np.array([chi1_l,chi2_l,chip,thetaJ,m1SI,m2SI,distance,alpha0,phic,fref]).tofile(inputs_file)
+    # Load test reference dataset's inputs
+    inputs_file = open(lal_cuda.full_path_datafile("inputs.dat"), "rb")
+    inputs_ref=np.fromfile(inputs_file,dtype=inputs_runtime.dtype,count=len(inputs_runtime))
+    freqs_ref=np.fromfile(inputs_file,dtype=freqs.dtype,count=len(freqs))
     inputs_file.close()
 
-    # Write results to binary files
-    freqs_file  = open("./freqs.dat", "wb")
-    freqs.tofile(freqs_file)
-    freqs_file.close()
+    # Test if the reference test dataset is the same as the run-time dataset
+    if(not np.array_equal(inputs_ref,inputs_runtime)):
+        flag_valid_ref = False
+    if(not np.array_equal(freqs_ref,freqs)):
+        flag_valid_ref = False
 
-    hp_val_file = open("./hp_val.dat", "wb")
-    hp_val.tofile(hp_val_file)
-    hp_val_file.close()
+    # Run test if we are running with the same inputs as the reference dataset
+    if(not flag_valid_ref):
+        print('Inputs differ from reference ... skipping test.')
+    else:
+        print('Performing test...')
 
-    hc_val_file = open("./hc_val.dat", "wb")
-    hc_val.tofile(hc_val_file)
-    hc_val_file.close()
+        # Read reference dataset's outputs
+        outputs_file = open(lal_cuda.full_path_datafile("outputs.dat"), "rb")
+        hp_ref=np.fromfile(outputs_file,dtype=hp_val.dtype,count=n_freq)
+        hc_ref=np.fromfile(outputs_file,dtype=hc_val.dtype,count=n_freq)
+        outputs_file.close()
+
+        # Compute statistics of difference from test reference
+        hpval_real_diff_avg = 0.
+        hpval_imag_diff_avg = 0.
+        hcval_real_diff_avg = 0.
+        hcval_imag_diff_avg = 0.
+        hpval_real_diff_max =-1e10 
+        hpval_imag_diff_max =-1e10 
+        hcval_real_diff_max =-1e10 
+        hcval_imag_diff_max =-1e10 
+        for (hp_val_i,hc_val_i,hp_ref_i,hc_ref_i) in zip(hp_val,hc_val,hp_ref,hc_ref):
+            hpval_real_diff_i = math.fabs((hp_val_i.real-hp_ref_i.real)/hp_val_i.real)
+            hpval_imag_diff_i = math.fabs((hp_val_i.imag-hp_ref_i.imag)/hp_val_i.imag)
+            hcval_real_diff_i = math.fabs((hc_val_i.real-hc_ref_i.real)/hc_val_i.real)
+            hcval_imag_diff_i = math.fabs((hc_val_i.imag-hc_ref_i.imag)/hc_val_i.imag)
+            hpval_real_diff_avg += hpval_real_diff_i
+            hpval_imag_diff_avg += hpval_imag_diff_i
+            hcval_real_diff_avg += hcval_real_diff_i
+            hcval_imag_diff_avg += hcval_imag_diff_i
+            hpval_real_diff_max = max([hpval_real_diff_max,hpval_real_diff_i])
+            hpval_imag_diff_max = max([hpval_imag_diff_max,hpval_imag_diff_i])
+            hcval_real_diff_max = max([hcval_real_diff_max,hcval_real_diff_i])
+            hcval_imag_diff_max = max([hcval_imag_diff_max,hcval_imag_diff_i])
+        hpval_real_diff_avg /= float(len(hp_val)) 
+        hpval_imag_diff_avg /= float(len(hp_val)) 
+        hpval_real_diff_avg /= float(len(hp_val)) 
+        hpval_imag_diff_avg /= float(len(hp_val)) 
+
+        # Report results
+        print('   Average/maximum real(hp_val) fractional difference: %.2e/%.2e'%(hpval_real_diff_avg,hpval_real_diff_max))
+        print('   Average/maximum imag(hp_val) fractional difference: %.2e/%.2e'%(hpval_imag_diff_avg,hpval_imag_diff_max))
+        print('   Average/maximum real(hc_val) fractional difference: %.2e/%.2e'%(hcval_real_diff_avg,hcval_real_diff_max))
+        print('   Average/maximum imag(hx_val) fractional difference: %.2e/%.2e'%(hcval_imag_diff_avg,hcval_imag_diff_max))
+
+        print("Done.")
+
+    # Write results to screen
+    if(write2stdout):
+        for [f_i,hp_i,hc_i] in zip(freqs,hp_val,hc_val):
+            print(f_i,hp_i.real,hp_i.imag,hc_i.real,hc_i.imag)
+
+    # Write to binary file
+    if(write2bin):
+        # Write inputs
+        print("Writting inputs to 'inputs.dat'...",end='')
+        inputs_file  = open("./inputs.dat", "wb")
+        inputs_runtime.tofile(inputs_file)
+        freqs.tofile(inputs_file)
+        inputs_file.close()
+        print("Done.")
+
+        # Write results
+        print("Writting outputs to 'outputs.dat'...",end='')
+        outputs_file = open("./outputs.dat", "wb")
+        hp_val.tofile(outputs_file)
+        hc_val.tofile(outputs_file)
+        outputs_file.close()
+        print("Done.")
 
 # Permit script execution
 if __name__ == '__main__':
